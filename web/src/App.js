64f2ad9ffc732bd4faff8a0e0024903fa048ccc0
Russell Kennington <arussellk@gmail.com>
2017-10-27 16:32:44 -0600

    Add routing to web

--- web/src/App.js
+++ web/src/App.js
@@ -5,21 +5,57 @@ import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 
+import {
+  BrowserRouter,
+  Route,
+  Link,
+} from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
+      <BrowserRouter>
+        <MuiThemeProvider>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
           </p>
-        </div>
-        <RaisedButton label="Default" />
-      </MuiThemeProvider>
+          </div>
+          <Link to="/">
+            <RaisedButton label="Home" />
+          </Link>
+          <Link to="/foo">
+            Foo
+          </Link>
+
+          <Route exact path="/" component={Home} />
+          <Route exact path="/foo" component={Foo} />
+        </MuiThemeProvider>
+      </BrowserRouter>
+    );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        home
+      </div>
+    );
+  }
+}
+
+class Foo extends Component {
+  render() {
+    return (
+      <div>
+        foo
+      </div>
     );
   }
 }
