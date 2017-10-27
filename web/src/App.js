0cc917a25c8bcbf457f7bdfc4c8fbbc24df08c41
Russell Kennington <arussellk@gmail.com>
2017-10-27 17:14:24 -0600

    Add Header

--- web/src/App.js
+++ web/src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BrowserRouter } from 'react-router-dom';
 
+import './styles/reset.css';
 import Routes from './Routes';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <MuiThemeProvider>
-          <Routes />
+          <div>
+            <Header />
+            <Routes />
+          </div>
         </MuiThemeProvider>
       </BrowserRouter>
     );
