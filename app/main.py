a56efd27b25a20e555c29be79b0336820b205341
williamjli <williamjli@gmail.com>
2017-10-28 10:24:16 -0600

    Removed unneeded route

--- app/main.py
+++ app/main.py
@@ -81,18 +81,6 @@ def index():
     </form>
     '''
 
-@app.route('/postcourse')
-def index2():
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post action="/api/courses" enctype=multipart/form-data>
-      <p><input type=text name=name>
-         <input type=submit value=Create>
-    </form>
-    '''
-
 @app.route('/api/courses', methods=['GET', 'POST', 'DELETE'])
 def courses():
     if request.method == 'GET':
