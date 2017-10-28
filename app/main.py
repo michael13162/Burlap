1cd672476d5578d7251becb06d2559e8511dba33
Russell Kennington <arussellk@gmail.com>
2017-10-27 23:02:45 -0600

    Add more CORS headers

--- app/main.py
+++ app/main.py
@@ -27,6 +27,8 @@ app = Flask(__name__, static_folder='static')
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
 
 ###############################################
